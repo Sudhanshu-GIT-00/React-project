1b37f8b4efee5de37fc6ad54b57f1b239cee41a8
Sudhanshu Ranjan <rsudhanshu620@gmail.com>
2024-06-08 23:57:14 +0530

    taiwind

---
+++ clone-youtube/src/components/VideoCard.tsx
@@ -0,0 +1,20 @@
+
+
+export function VideoCard() {
+  return (
+    <div>
+        <img src="/image.png" alt="hallo" />
+        <div className="grid grid-cols-12">
+            <div className="col-span-2">
+                <img src="/image.png" alt="" />
+            </div>
+            <div className="col-span-10">
+                home: mera ghar(ma)
+                i love you
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
