8da26013b857ebdc6142f0b83703145b9726fad4
Sudhanshu Ranjan <rsudhanshu620@gmail.com>
2024-06-09 01:36:08 +0530

    add the thumbnail one component

--- clone-youtube/src/components/VideoCard.tsx
+++ clone-youtube/src/components/VideoCard.tsx
@@ -1,19 +1,21 @@
-
-
-export function VideoCard() {
+export function VideoCard(props: any) {
   return (
     <div>
-        <img src="/image.png" alt="hallo" />
-        <div className="grid grid-cols-12">
-            <div className="col-span-2 rounded-xl">
-                <img className={"rounded-full"} src="/image.png" alt="" />
-            </div>
-            <div className="col-span-10">
-                home: mera ghar(ma)
-                i love you
-            </div>
+      <img src={props.image} className="rounded-xl w-150 h-100" alt="hallo" />
+      <div className="grid grid-cols-12 pt-2">
+        <div className="col-span-1 rounded-xl">
+          <img className={"rounded-full w-12 h-12"} src={props.thumbImage} alt="" />
         </div>
+        <div className="col-span-10 pl-5">
+          <div>{props.title}</div>
+          <div className="col-span-10 text-gray-400 text-base">
+            {props.author}
+          </div>
+          <div className="col-span-10 text-gray-400 text-base">
+          {props.view} | {props.timestamp}
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
-
--- clone-youtube/src/pages/index.tsx
+++ clone-youtube/src/pages/index.tsx
@@ -7,7 +7,14 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <div>
-      <VideoCard/>
+      <VideoCard 
+      title={"There are some video | 30 day plan | play and watch"}
+      image={"image.png"}
+      thumbImage={"thumb.png"}
+      timestamp={"30 day ago"}
+      author={"Aman Raja"}
+      view={"500M"}
+      />
     </div>
   );
 }
