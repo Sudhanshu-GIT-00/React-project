27d8c10b4c9354355c6bce7dbfd91e21164ab3ed
Sudhanshu Ranjan <rsudhanshu620@gmail.com>
2024-06-09 00:33:28 +0530

    taiwind problem solve

--- clone-youtube/src/components/VideoCard.tsx
+++ clone-youtube/src/components/VideoCard.tsx
@@ -5,14 +5,13 @@ export function VideoCard() {
     <div>
         <img src="/image.png" alt="hallo" />
         <div className="grid grid-cols-12">
-            <div className="col-span-2">
-                <img src="/image.png" alt="" />
+            <div className="col-span-2 rounded-xl">
+                <img className={"rounded-full"} src="/image.png" alt="" />
             </div>
             <div className="col-span-10">
                 home: mera ghar(ma)
                 i love you
             </div>
-
         </div>
     </div>
   )
