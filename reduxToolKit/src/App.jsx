d5f1bb437092f43a1c146fe1151b0db280a379d2
Sudhanshu Ranjan <rsudhanshu620@gmail.com>
2024-06-11 21:04:05 +0530

    some toolkit of redux how use in react

--- reduxToolKit/src/App.jsx
+++ reduxToolKit/src/App.jsx
@@ -1,12 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import AddTodo from './components/AddTodo.jsx'
 import Todos from './components/Todos.jsx'
 
 function App() {
-
   return (
     <>
       <h1>Learn about redux toolkit</h1>
